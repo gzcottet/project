b9273230d72319e182665bdb073c987d7eda12d5
gzcottet <gzcottet@minha.fag.edu.br>
2024-09-20 11:49:00 -0300

    Corrige caminho para solutions.css

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,8 @@ import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection';
 import Solutions from '../components/Solutions';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-import '../styles/solutions.css'; // Para o componente Solutions
+import '../styles/solutions.css';
+
 
 const App = () => {
   return (
